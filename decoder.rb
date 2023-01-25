5f153e0f7763c8e82c8eec3c992e2d08e2804dfe
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2023-01-25 17:53:12 +0200

    Fix linter errors

--- decoder.rb
+++ decoder.rb
@@ -1,62 +1,62 @@
 def decode_char(char)
-    morsdict = {
-        '.-' => 'A',
-        '-...'  => 'B',
-        '-.-.' => 'C',
-        '-..' => 'D',
-        '.'  =>  'E',
-        '..-.' => 'F',
-        '--.' => 'G',
-        '....' => 'H',
-        '..' => 'I',
-        '.---'  => 'J',
-        '-.-' => 'K',
-        '.-..' => 'L',
-        '--' => 'M',
-        '-.'  => 'N',
-        '---' => 'O',
-        '.--.' => 'P',
-        '--.-' => 'Q',
-        '.-.' => 'R',
-        '...' => 'S',
-        '-' => 'T',
-        '..-' => 'U',
-        '...-' => 'V',
-        '.--' => 'W',
-        '-..-' => 'X',
-        '-.--' => 'Y',
-        '--..' => 'Z',
-        '-----' => '0',
-        '.----' => '1',
-        '..---' => '2',
-        '...--' => '3',
-        '....-' => '4',
-        '.....' => '5',
-        '-....' => '6',
-        '--...' => '7',
-        '---..' => '8',
-        '----.' => '9',
-        '.-.-.-' => 'Fullstop',
-        '--..--' => 'Comma',
-        '..--..' => 'Query'
-    }
+  morsdict = {
+    '.-' => 'A',
+    '-...' => 'B',
+    '-.-.' => 'C',
+    '-..' => 'D',
+    '.' => 'E',
+    '..-.' => 'F',
+    '--.' => 'G',
+    '....' => 'H',
+    '..' => 'I',
+    '.---' => 'J',
+    '-.-' => 'K',
+    '.-..' => 'L',
+    '--' => 'M',
+    '-.' => 'N',
+    '---' => 'O',
+    '.--.' => 'P',
+    '--.-' => 'Q',
+    '.-.' => 'R',
+    '...' => 'S',
+    '-' => 'T',
+    '..-' => 'U',
+    '...-' => 'V',
+    '.--' => 'W',
+    '-..-' => 'X',
+    '-.--' => 'Y',
+    '--..' => 'Z',
+    '-----' => '0',
+    '.----' => '1',
+    '..---' => '2',
+    '...--' => '3',
+    '....-' => '4',
+    '.....' => '5',
+    '-....' => '6',
+    '--...' => '7',
+    '---..' => '8',
+    '----.' => '9',
+    '.-.-.-' => 'Fullstop',
+    '--..--' => 'Comma',
+    '..--..' => 'Query'
+  }
 
-    return morsdict[char]
+  morsdict[char]
 end
 
 def decode_word(word)
-    morsWord = ''
-    myWordArray = word.split
-    myWordArray.each { |x| morsWord +=  decode_char(x) }
-    return morsWord
+  mors_Word = ''
+  myWordArray = word.split
+  myWordArray.each { |x| mors_Word += decode_char(x) }
+  mors_Word
 end
 
 def decode(words)
-    mySentence = ''
-    words = words.split('   ')
-    words.each { |x| mySentence += decode_word(x) + ' '}
-    return mySentence.chop
+  my_Sentence = ''
+  words = words.split('   ')
+  words.each { |x| my_Sentence += "#{decode_word(x)} " }
+  my_Sentence.chop
 end
 
-morsSentence = decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
-puts morsSentence
+mors_Sentence = decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
+puts mors_Sentence
