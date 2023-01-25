544e9740170e7b4ab66f5caa504ba7628a6746b5
Andrea Tamara Diegaardt <atdiegaardt@gmail.com>
2023-01-25 14:48:51 +0200

    Create a method to decode a Morse code character,
    takes a string parameter, and return the corresponding character in uppercase (e.g. decode_char(".-") returns "A").

--- decoder.rb
+++ decoder.rb
@@ -1,1 +1,36 @@
+ALPHABET = {
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
+}.freeze
 
+# Create a method to decode a Morse code character, takes a string parameter, and return the corresponding character in uppercase (e.g. decode_char(".-") returns "A").
+def decode_char(code_char)
+  ALPHABET[code_char]
+end
+  
+puts decode_char('.-')
+  
